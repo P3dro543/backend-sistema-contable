c3543538bafafa57a2ba0f2a5dc255588e297b64
Fela-Dev <er1280279@gmail.com>
2026-04-28 11:21:28 -0600

    hola

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('API funcionando 🚀');
+    res.send('API funcionando 🚀😎');
 });
 app.get('/test', (req, res) => {
   res.send('test funcionando');
